59ba12d8339c45501f9e404f67608855afcad26c
YorickH <yorick@teamleader.eu>
2024-12-12 22:52:09 +0100

    Add env variables to configure the dev server

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: process.env.VUE_APP_HOST || 'localhost',
+    port: process.env.VUE_APP_PORT || 8080
+  }
 })
